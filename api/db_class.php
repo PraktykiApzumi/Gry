681c3f446607ddce9febdf06c075be5317db9a29
Michał Kałasiewicz <michalkalasiewicz0@gmail.com>
2026-05-07 14:55:32 +0200

    DB handle - singleton, konstruktor, angielski, nazwane parametry w SQLu

--- api/db_class.php
+++ api/db_class.php
@@ -1,40 +1,72 @@
 <?php
-class Dbh{
-    private $host;
-    private $user;
-    private $pwd;
-    private $dbName;
 
-    protected function connect(){
-        $this->host = "localhost";
-        $this->user = "root";
-        $this->pwd = "";
-        $this->dbName = "planszowka";
+class DatabaseHandle {
 
-        $dsn = "mysql:host=".$this->host.";dbname=".$this->dbName;
-        $pdo = new PDO($dsn, $this->user, $this->pwd);
+    private static ?DatabaseHandle $instance = null;
+    private PDO $connection;
 
+    private function __construct($host, $dbname, $username, $password) {
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+        try {
+            $this->connection = new PDO($dsn, $username, $password);
+        } catch (PDOException $e) {
+            die("connection error: " . $e->getMessage());
+        }
     }
-    function dodaj_gracza($imie, $nazwisko){
-        $sql = "INSERT INTO gracze (imie, nazwisko) VALUES (?, ?)";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$imie, $nazwisko]);
+
+    public static function getInstance($host, $dbname, $username, $password): DatabaseHandle {
+        if (self::$instance === null) {
+            self::$instance = new DatabaseHandle($host, $dbname, $username, $password);
+        }
+        return self::$instance;
     }
-    function dodaj_gre($nazwa, $rodzaj,$min_graczy, $max_graczy, $rodzaj_wygranej){
-        $sql = "INSERT INTO gry (nazwa, rodzaj, min_graczy, max_graczy, rodzaj_wygranej) VALUES (?, ?, ?, ?, ?)";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$nazwa, $rodzaj, $min_graczy, $max_graczy, $rodzaj_wygranej]);
+
+    public function addPlayer($nickname) {
+        $sql = "INSERT INTO gracze (nick) VALUES (:nickname)";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            'nickname' => $nickname,
+        ]);
     }
-    function dodaj_rozgrywke($id_zwyciezcy, $id_gry, $data_rozgrywki, $wynik){
-        $sql = "INSERT INTO rozgrywki (id_zwyciezcy, id_gry, data_rozgrywki, wynik) VALUES (?, ?, ?, ?)";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$id_zwyciezcy, $id_gry, $data_rozgrywki, $wynik]);
+
+    public function addGame($name, $type, $minPlayers, $maxPlayers, $winType) {
+        $sql = "INSERT INTO gry (nazwa, rodzaj, min_graczy, max_graczy, rodzaj_wygranej)
+                VALUES (:name, :type, :min_players, :max_players, :win_type)";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            'name' => $name,
+            'type' => $type,
+            'min_players' => $minPlayers,
+            'max_players' => $maxPlayers,
+            'win_type' => $winType
+        ]);
     }
-    function dodaj_wynik($id_rozgrywki, $id_gracza, $punkty){
-        $sql = "INSERT INTO wyniki (id_rozgrywki, id_gracza, punkty) VALUES (?, ?, ?)";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$id_rozgrywki, $id_gracza, $punkty]);
+
+    public function addMatch($winnerId, $gameId, $matchDate, $result) {
+        $sql = "INSERT INTO rozgrywki (id_zwyciezcy, id_gry, data_rozgrywki, wynik)
+                VALUES (:winner_id, :game_id, :match_date, :result)";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            'winner_id' => $winnerId,
+            'game_id' => $gameId,
+            'match_date' => $matchDate,
+            'result' => $result
+        ]);
     }
-    
+
+    public function addScore($matchId, $playerId, $points) {
+        $sql = "INSERT INTO wyniki (id_rozgrywki, id_gracza, punkty)
+                VALUES (:match_id, :player_id, :points)";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            'match_id' => $matchId,
+            'player_id' => $playerId,
+            'points' => $points
+        ]);
+    }
 }
-?>
