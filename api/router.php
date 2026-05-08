cc09d20e5f715474621a39def75fe4a4916b8e38
mateusz kuchowicz <mateusz.kuchowicz.ofic@gmail.com>
2026-05-08 08:57:41 +0200

    feat: dodaj modal dodawania gry i przycisk +++

---
+++ api/router.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once __DIR__ . '/handlers.php';
+
+$routes = require __DIR__ . '/routes.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+$path = parse_url($uri, PHP_URL_PATH);
+$path = str_replace('/gry/public', '', $path);
+
+if ($path === '') {
+    $path = '/';
+}
+
+function matchRoute($routes, $method, $path)
+{
+    if (!isset($routes[$method])) {
+        return null;
+    }
+
+    $urlParts = explode('/', trim($path, '/'));
+
+    foreach ($routes[$method] as $route => $handler) {
+        $routeParts = explode('/', trim($route, '/'));
+
+        if (count($routeParts) !== count($urlParts)) {
+            continue;
+        }
+
+        $params = [];
+        $match = true;
+
+        foreach ($routeParts as $i => $part) {
+            if (str_starts_with($part, '{') && str_ends_with($part, '}')) {
+                $name = trim($part, '{}');
+                $params[$name] = $urlParts[$i];
+            } elseif ($part !== $urlParts[$i]) {
+                $match = false;
+                break;
+            }
+        }
+
+        if ($match) {
+            return [$handler, $params];
+        }
+    }
+
+    return null;
+}
+
+$result = matchRoute($routes, $method, $path);
+
+if ($result) {
+    [$handler, $params] = $result;
+
+    if (is_callable($handler)) {
+        $handler($params);
+        exit;
+    }
+}
+
+jsonResponse([
+    'ok' => false,
+    'errorCode' => 404,
+    'errorName' => 'NotFound',
+], 404);
